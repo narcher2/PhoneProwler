4283541b87cfdc91b78b01bfc31c37430e00a3e7
narcher2 <narcher2@gmu.edu>
2015-02-26 15:47:01 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,11 +19,29 @@ function create() {
 
     background = game.add.tileSprite(0, 0, 800, 600, 'background');
 
-    button = game.add.button(44, 400, 'button', actionOnClick, this, 2, 1, 0);
+    button = game.add.button(32, 400, 'button', actionOnClick, this, 2, 1, 0);
 
     button.onInputOver.add(over, this);
     button.onInputOut.add(out, this);
     button.onInputUp.add(up, this);
+    
+    button2 = game.add.button(32, 448, 'button', actionOnClick, this, 2, 1, 0);
+
+    button2.onInputOver.add(over, this);
+    button2.onInputOut.add(out, this);
+    button2.onInputUp.add(up, this);
+    
+    button3 = game.add.button(32, 496, 'button', actionOnClick, this, 2, 1, 0);
+
+    button3.onInputOver.add(over, this);
+    button3.onInputOut.add(out, this);
+    button3.onInputUp.add(up, this);
+    
+    button4 = game.add.button(32, 540, 'button', actionOnClick, this, 2, 1, 0);
+
+    button4.onInputOver.add(over, this);
+    button4.onInputOut.add(out, this);
+    button4.onInputUp.add(up, this);
     
         text = game.add.text(game.world.centerX, game.world.centerY,"Take a seat Mr. Player.\nLet's get started.", {
         font: "40px Arial",
@@ -38,10 +56,36 @@ function create() {
     var t = game.add.text(game.world.centerX-300, 0, text, style);
     */
     
-    var buttonText1 = "Take a seat";
-    var buttonStyle = {font: "20px Arial", fill: "#000000", align: "left" };
+    buttonText1 = game.add.text(60, 400,"Take a seat", {
+        font: "20px Arial",
+        fill: "#000000",
+        align: "left"
+    });
     
-    var bt1 = game.add.text(60, 400, buttonText1, buttonStyle)
+    buttonText2 = game.add.text(60, 448,"Take a seat", {
+        font: "20px Arial",
+        fill: "#000000",
+        align: "left"
+    });
+    
+    buttonText3 = game.add.text(60, 496,"Take a seat", {
+        font: "20px Arial",
+        fill: "#000000",
+        align: "left"
+    });
+    
+    buttonText4 = game.add.text(60, 544,"Take a seat", {
+        font: "20px Arial",
+        fill: "#000000",
+        align: "left"
+    });
+    
+    //var buttonText1 = "Take a seat";
+    //var buttonStyle = {font: "20px Arial", fill: "#000000", align: "left" };
+    
+    //var bt1 = game.add.text(60, 400, buttonText1, buttonStyle)
+    
+
 
 }
 
