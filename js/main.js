9ffc6c2f3e5b1f2094a2af1c6f43d193ccab2f0e
narcher2 <narcher2@gmu.edu>
2015-02-26 13:28:03 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,38 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { create: create });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create });
+
+function preload() {
+
+    game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 512, 48);
+
+}
+
+var button;
+var background;
 
 function create() {
 
-    var text = "It's nice to meet you Mr. Player. Take a seat, and we can get started.";
+    var text = "It's nice to meet you Mr. Player.\nTake a seat, and we can get started.";
     var style = { font: "44px Arial", fill: "#ff0044", align: "center" };
 
     var t = game.add.text(game.world.centerX-300, 0, text, style);
+    
+    button = game.add.button(game.world.centerX - 32, 400, 'button', actionOnClick, this, 2, 1, 0);
+
+    button.onInputOver.add(over, this);
+    button.onInputOut.add(out, this);
+    button.onInputUp.add(up, this);
 
 }
+
+function up() {
+    console.log('button up', arguments);
+}
+
+function over() {
+    console.log('button over');
+}
+
+function out() {
+    console.log('button out');
+}
+
