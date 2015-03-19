52b72d639ecc2712e3e71a6b46e2d81d88b23012
narcher2 <narcher2@gmu.edu>
2015-03-19 09:20:37 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -34,25 +34,25 @@ function create() {
     //textbox1 = game.add.button(170, 76, 'textboxblack', actionOnClick, this, 2, 1, 0);
     textbox1 = game.add.tileSprite(170, 76, 835, 392, 'textboxblack');
 
-    button = game.add.button(32, 400, 'button', actionOnClick, this, 2, 1, 0);
+    button = game.add.button(170, 468, 'button', actionOnClick, this, 2, 1, 0);
 
     button.onInputOver.add(over, this);
     button.onInputOut.add(out, this);
     button.onInputUp.add(up, this);
     
-    button2 = game.add.button(32, 448, 'button', actionOnClick, this, 2, 1, 0);
+    //button2 = game.add.button(32, 448, 'button', actionOnClick, this, 2, 1, 0);
 
     button2.onInputOver.add(over, this);
     button2.onInputOut.add(out, this);
     button2.onInputUp.add(up, this);
     
-    button3 = game.add.button(32, 496, 'button', actionOnClick, this, 2, 1, 0);
+    //button3 = game.add.button(32, 496, 'button', actionOnClick, this, 2, 1, 0);
 
     button3.onInputOver.add(over, this);
     button3.onInputOut.add(out, this);
     button3.onInputUp.add(up, this);
     
-    button4 = game.add.button(32, 540, 'button', actionOnClick, this, 2, 1, 0);
+    //button4 = game.add.button(32, 540, 'button', actionOnClick, this, 2, 1, 0);
 
     button4.onInputOver.add(over, this);
     button4.onInputOut.add(out, this);
