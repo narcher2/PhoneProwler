f8c5534bd1d3652df6abffd6e972a8196d6fd542
narcher2 <narcher2@gmu.edu>
2015-03-19 09:13:12 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function preload() {
     game.load.image('phone','assets/buttons/phonebackground.png');
     game.load.audio('chopin', 'assets/Prelude15.mp3');
     game.load.image('background','assets/background.png');
-    game.load.image('textboxblack','assets/buttons/textbox.png');
+    game.load.image('textboxblack','assets/buttons/tealtextbox.png');
 
 
 }
@@ -31,7 +31,8 @@ function create() {
     //player = game.add.sprite(400, 500, 'ship');
     //phone = game.add.sprite( game.world.centerX, game.world.centerY, 'phone', assets/buttons/phonebackground.png');
     
-    textbox1 = game.add.button(170, 76, 'textboxblack', actionOnClick, this, 2, 1, 0);
+    //textbox1 = game.add.button(170, 76, 'textboxblack', actionOnClick, this, 2, 1, 0);
+    textbox1 = game.add.sprite(0, 0, 'textboxblack');
 
     button = game.add.button(32, 400, 'button', actionOnClick, this, 2, 1, 0);
 
