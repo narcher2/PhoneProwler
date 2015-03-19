0058f7e054eda6168c427117de9ad13d5e64f22d
narcher2 <narcher2@gmu.edu>
2015-03-19 09:31:39 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -35,7 +35,7 @@ function create() {
     //textbox1 = game.add.button(170, 76, 'textboxblack', actionOnClick, this, 2, 1, 0);
     textbox1 = game.add.tileSprite(170, 76, 835, 392, 'textboxblack');
 
-    button = game.add.button(170, 468, 'messages', actionOnClick, this, 1, 0, 1);
+    button = game.add.button(170, 468, 'messages', actionOnClick, this, 0, 0, 1);
 
     button.onInputOver.add(over, this);
     button.onInputOut.add(out, this);
