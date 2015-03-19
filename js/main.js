7bf3e3b51242bd57e0fadcb10551e2df77444ff1
narcher2 <narcher2@gmu.edu>
2015-03-19 12:32:37 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -64,7 +64,7 @@ function create() {
     //phone = game.add.sprite( game.world.centerX, game.world.centerY, 'phone', assets/buttons/phonebackground.png');
     
     //textbox1 = game.add.button(170, 76, 'textboxblack', actionOnClick, this, 2, 1, 0);
-    textbox1 = game.add.tileSprite(170, 76, 835, 392, 'textboxblack');
+    //textbox1 = game.add.tileSprite(170, 76, 835, 392, 'textboxblack');
 
     button = game.add.button(170, 468, 'messages', actionOnClick, this, 0, 0, 1);
 
